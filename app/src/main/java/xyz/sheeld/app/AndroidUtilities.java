3a4610f4a13f89f96b28f358a2858f39c070af1f
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-05-01 12:34:30 +0530

    chore: added fonts

--- app/src/main/java/xyz/sheeld/app/AndroidUtilities.java
+++ app/src/main/java/xyz/sheeld/app/AndroidUtilities.java
@@ -1,11 +1,33 @@
 package xyz.sheeld.app;
 
+import android.content.Context;
+import android.graphics.Typeface;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import java.util.Hashtable;
+
 public class AndroidUtilities {
+    private static final Hashtable<String, Typeface> typefaceCache = new Hashtable<>();
+
     public static float density = 1;
     public static int dp(float value) {
         if (value == 0) {
             return 0;
         }
         return (int) Math.ceil(density * value);
+    }
+
+    public static Typeface getLightTypeface(Context context) {
+        return ResourcesCompat.getFont(context, R.font.poppins_light);
+    }
+    public static Typeface getRefularTypeface(Context context) {
+        return ResourcesCompat.getFont(context, R.font.poppins_regular);
+    }
+    public static Typeface getMediumTypeface(Context context) {
+        return ResourcesCompat.getFont(context, R.font.poppins_medium);
+    }
+    public static Typeface getSemiBoldTypeface(Context context) {
+        return ResourcesCompat.getFont(context, R.font.poppins_semi_bold);
     }
 }
